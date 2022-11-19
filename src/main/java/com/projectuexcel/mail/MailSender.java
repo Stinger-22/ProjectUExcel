f1283b680355811f27c15166ed20991b22c089ab
Stinger <maxym.sobol@gmail.com>
2022-11-19 22:37:10 +0200

    Customized user interface

--- src/main/java/com/projectuexcel/mail/MailSender.java
+++ src/main/java/com/projectuexcel/mail/MailSender.java
@@ -72,7 +72,7 @@ public class MailSender {
         Message message = new MimeMessage(session);
         message.setFrom(new InternetAddress(senderMail));
         message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(receiver));
-        sendMessage(message, subject, text);
+//        sendMessage(message, subject, text);
     }
 
     public void sendMessageAttachment(String[] receivers, String subject, String text) throws MessagingException, IOException {
@@ -84,7 +84,7 @@ public class MailSender {
         for (String receiver : receivers) {
             message.addRecipients(Message.RecipientType.TO, InternetAddress.parse(receiver));
         }
-        sendMessage(message, subject, text);
+//        sendMessage(message, subject, text);
     }
 
     private void sendMessage(Message message, String subject, String text) throws MessagingException, IOException {
--- src/main/java/com/projectuexcel/ui/SendAllController.java
+++ src/main/java/com/projectuexcel/ui/SendAllController.java
@@ -7,7 +7,6 @@ import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
